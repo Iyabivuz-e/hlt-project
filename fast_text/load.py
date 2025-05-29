77a276cbdb77ec8938c5aae6a3931fbc477721a4
Matteo Piredda <73900156+MatteoPirii@users.noreply.github.com>
2025-05-29 23:50:32 +0200

    Revert "read from drive instead of download"

--- fast_text/load.py
+++ fast_text/load.py
@@ -1,11 +1,16 @@
 import fasttext
+import gdown
 import os
 
+FASTTEXT_BINARY_ID = "1_iCbXY1yyZ536iNnzowJQFzJ9sSVvz4J"
+FASTTEXT_MULTICLASS_ID = "1-E62aKmSF04OnA_8112XQwhFAUR2Uf3s"
 
 def load_fasttext(mode: str):
-    path = f"/content/drive/MyDrive/HLT_artifacts_group_5/fine-tuned_models/fasttext-{mode}.bin"
-    return fasttext.load_model(path)
+    os.makedirs("models", exist_ok=True)
+    output_path = f"models/fasttext_{mode}.bin"
 
-if __name__ == "__main__":
-    model = load_fasttext("binary")
-
+    if not os.path.exists(output_path):
+        file_id = FASTTEXT_MULTICLASS_ID if mode != "binary" else FASTTEXT_BINARY_ID
+        gdown.download(f"https://drive.google.com/uc?id={file_id}", output_path, quiet=False)
+    
+    return fasttext.load_model(output_path)
